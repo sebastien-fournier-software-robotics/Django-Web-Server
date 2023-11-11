a8403209026fc300b3b7eaf8cf0b049c8de2298b
Seb Fournier <sebastien.fournier@up-technologies.fr>
2023-11-11 15:24:38 +0100

    Update of field protocol + display urls strings

--- myProject/part1/models.py
+++ myProject/part1/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Url(models.Model):
     url_string = models.URLField()
     domain_name = models.CharField(max_length=255)
-    protocol = models.CharField(max_length=5)
     title = models.CharField(max_length=255)
     image = models.JSONField(null=True, blank=True)
     stylesheets = models.PositiveIntegerField(default=0)
+
+    class protocol(models.TextChoices):
+        HTTP = "H"
+        HTTPS = "HS"
+
+    protocol = models.fields.CharField(choices=protocol.choices, max_length=5)
--- myProject/part1/views.py
+++ myProject/part1/views.py
@@ -1,27 +1,10 @@
-# from django.shortcuts import render
-
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 from part1.models import Url
 
 
 def hello(request):
-    urls = Url.objects.all()
+    url_list = Url.objects.all()
 
-    if urls:
-        url_list = "<ul>"
-        for url in urls:
-            url_list += f"<li>{url.url_string}</li>"
-        url_list += "</ul>"
-    else:
-        url_list = "No URLs available."
-
-    return HttpResponse(
-        f"""
-        <h1>Hello Django !</h1>
-        <p>Mes urls sont :</p>
-        {url_list}
-        """
-    )
+    return render(request, "part1/hello.html", {"urls": url_list})
