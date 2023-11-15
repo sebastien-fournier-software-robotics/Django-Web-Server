28b7d1a16f29621ab16db5f5280d345cb484c9f9
Seb Fournier <sebastien.fournier@up-technologies.fr>
2023-11-15 01:33:39 +0100

    DELETE request update

--- myProject/part1/views.py
+++ myProject/part1/views.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -52,3 +52,8 @@ class UrlViewSet(viewsets.ModelViewSet):
             return Response({"error": str(e)}, status=400)
         except ValidationError as e:
             return Response({"error": str(e)}, status=400)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
